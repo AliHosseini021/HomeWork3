fb92ca62549f34476b8a88bb3706b7815613fb22
AliHosseini021 <55647390+AliHosseini021@users.noreply.github.com>
2020-05-22 12:14:54 +0430

    Delete findItems.js

--- findItems.js
+++
@@ -1,8 +0,0 @@
-///question 4
-let findItem = (array = [], item) => {
-  let ans = { index: -1, item: item };
-  for (let i in array) {
-    if (array[i] == item) ans.index = i;
-  }
-  return ans;
-};
